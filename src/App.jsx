79351a3b692d24e87c29bc477dfeee3c69c97c98
Federico Villace <federicovillace@gmail.com>
2023-01-26 20:50:06 -0300

    Added first useEffect to get the fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+// import { CAT_RANDOM_IMAGE_URL } from './utils/links'
 
 export const App = () => {
   const [fact, setfact] = useState('')
@@ -6,13 +7,18 @@ export const App = () => {
   useEffect(() => {
     fetch('https://catfact.ninja/fact')
       .then(response => response.json())
-      .then(data => setfact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setfact(data.fact)
+        const wordsSelected = fact.split(' ').slice(0, 3).join(' ')
+        console.log(wordsSelected)
+      })
   }, [])
 
   return (
     <main>
       <h1>Kittens App </h1>
-      {fact}
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
---
+++ src/utils/links.js
@@ -0,0 +1,1 @@
+export const CAT_RANDOM_IMAGE_URL = process.env.CAT_RANDOM_IMAGE_URL
