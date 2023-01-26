2c64fd4ef320894c6279e4832952ecb5c6c694ed
Federico Villace <federicovillace@gmail.com>
2023-01-26 20:31:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+  const [fact, setfact] = useState('')
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => setfact(data.fact))
+  }, [])
+
   return (
-    <div>App de gatitos</div>
+    <main>
+      <h1>Kittens App </h1>
+      {fact}
+    </main>
   )
 }
